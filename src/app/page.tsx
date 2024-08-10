c072ce9180371772f7ccf20be15fb726fd2dfa2b
Valentyn13 <valentyn13git@gmail.com>
2024-08-10 18:37:27 +0300

    check cn function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { cn } from '@/utils';
+
 const Home = () => {
     return (
-        <div>
+        <div className={cn('h-full', 'w-full', 'flex')}>
             <h1>Hellow world</h1>
         </div>
     );
